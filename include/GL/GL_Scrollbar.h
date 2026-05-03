230d2a1a6c2242b01dfd20d240f45be8fd37938c
testyourmine <25854988+testyourmine@users.noreply.github.com>
2026-05-03 09:39:22 -0500

    Document GL_Scrollbar

--- include/GL/GL_Scrollbar.h
+++ include/GL/GL_Scrollbar.h
@@ -3,20 +3,20 @@
 
 #include "global.h"
 
-void sub_08015384(struct Unk_08015384 *pThis, s32 arg1, s32 arg2, s32 arg3, s32 arg4, s32 arg5, s32 arg6, s32 arg7, s32 arg8);
-s32 sub_08015424(struct Unk_08015384 *pThis);
-s32 sub_0801544C(struct Unk_08015384 *pThis);
+void GL_InitScrollbar(struct Unk_08015384 *pThis, s32 baseXPos, s32 baseYPos, s32 unk_0_24, s32 scrollLength, s32 scrollCursorStartOffset, s32 scrollCursorEndOffset, s32 totalItems, s32 visibleItems);
+s32 GL_GetScrollCursorYPosition(struct Unk_08015384 *pThis);
+s32 GL_CalculateScrollCursorYPos(struct Unk_08015384 *pThis);
 s32 sub_08015480(struct Unk_08015384 *pThis);
-s32 sub_08015488(struct Unk_08015384 *pThis);
-void sub_08015490(struct Unk_08015384 *pThis, s32 arg1);
-s32 sub_080154A4(struct Unk_08015384 *pThis);
-void sub_080154C8(struct Unk_08015384 *pThis, s32 arg1);
-s32 sub_080154F0(struct Unk_08015384 *pThis);
-s32 sub_0801550C(struct Unk_08015384 *pThis);
-s32 sub_08015518(struct Unk_08015384 *pThis);
-s32 sub_08015550(struct Unk_08015384 *pThis);
-void sub_08015584(struct Unk_08015384 *pThis);
-void sub_080155C0(struct Unk_08015384 *pThis);
-void sub_080155F4(struct Unk_08015384 *pThis);
+s32 GL_GetUpdateCursorVisualFlag(struct Unk_08015384 *pThis);
+void GL_SetUpdateCursorVisualFlag(struct Unk_08015384 *pThis, s32 flag);
+s32 GL_GetScrollCursorIndex(struct Unk_08015384 *pThis);
+void GL_SetScrollCursorIndex(struct Unk_08015384 *pThis, s32 index);
+s32 GL_CheckScrollCursorNotAtEnd(struct Unk_08015384 *pThis);
+s32 GL_CheckScrollCursorNotAtBeginning(struct Unk_08015384 *pThis);
+s32 GL_IncreaseScrollCursorIndexByOne(struct Unk_08015384 *pThis);
+s32 GL_DecreaseScrollCursorIndexByOne(struct Unk_08015384 *pThis);
+void GL_IncreaseScrollCursorIndexByPage(struct Unk_08015384 *pThis);
+void GL_DecreaseScrollCursorIndexByPage(struct Unk_08015384 *pThis);
+void GL_UpdateScrollCursorYPosition(struct Unk_08015384 *pThis);
 
 #endif // GUARD_GL_SCROLLBAR_H
--- src/GL/GL_Scrollbar.c
+++ src/GL/GL_Scrollbar.c
@@ -1,191 +1,271 @@
 #include "global.h"
 
-struct Unk_08015384 {
-    u32 unk_0_0:1;
+struct ScrollBarInfo {
+    u32 updateCursorVisualFlag:1;
     u32 unk_0_1:1;
     u32 unk_0_2:4;
-    u32 unk_0_6:9;
-    u32 unk_0_15:9;
+    u32 baseXPos:9;
+    u32 baseYPos:9;
     u32 unk_0_24:8;
-    u16 unk_4; // current scroll index
-    u16 unk_6;
-    u8 unk_8;
-    u8 unk_9;
-    u8 unk_A;
-    u8 unk_B;
-    u32 unk_C_0:9;
-    u32 unk_C_9:9;
+    u16 scrollCursorIndex;
+    u16 totalItems;
+    u8 scrollLength; // in pixels
+    u8 scrollCursorStartOffset;
+    u8 scrollCursorEndOffset;
+    u8 visibleItems;
+    u32 unk_C_0:9; // unused, possibly scrollCursorXPos
+    u32 scrollCursorYPos:9;
 };
 
 extern void OSi_Panic(u8 *, s32, u8 *, s32);
 
-// 15384
-void sub_08015384(struct Unk_08015384 *pThis, s32 arg1, s32 arg2, s32 arg3, s32 arg4, s32 arg5, s32 arg6, s32 arg7, s32 arg8)
+/**
+ * @brief 15384 | Initialize scrollbar
+ * 
+ * @param pThis Pointer to scrollbar
+ * @param baseXPos Base X position of scrollbar
+ * @param baseYPos Base Y position of scrollbar
+ * @param unk_0_24 Unknown
+ * @param scrollLength Total length scroll cursor can travel in pixels
+ * @param scrollCursorStartOffset Scroll cursor offset from start
+ * @param scrollCursorEndOffset Scroll cursor offset from end
+ * @param totalItems Total number of items in page
+ * @param visibleItems Number of visible items at once
+ */
+void GL_InitScrollbar(struct ScrollBarInfo *pThis, s32 baseXPos, s32 baseYPos, s32 unk_0_24, s32 scrollLength, s32 scrollCursorStartOffset, s32 scrollCursorEndOffset, s32 totalItems, s32 visibleItems)
 {
     CpuFill32(0, pThis, 0x10);
-    pThis->unk_0_6 = arg1; // 0xE0 (base xPos?)
-    pThis->unk_0_15 = arg2; // 0x20 (base yPos)
-    pThis->unk_0_24 = arg3; // 0x8
-    pThis->unk_8 = arg4; // 0x52 (length of scroll in pixels)
-    pThis->unk_9 = arg5; // 0x6 (y offset from base yPos)
-    pThis->unk_A = arg6; // 0x6 (y offset from end of scroll)
-    pThis->unk_6 = arg7; // 0xD (some kind of limiting factor? unk_6 - unk_B = number of scrolls)
-    pThis->unk_B = arg8; // 0x5 (amount on screen to scroll by?)
-    pThis->unk_C_9 = arg2 + arg5; // 0x20 + 0x6 = 0x26 (yPos on screen)
-    pThis->unk_0_0 = 1; // update position on screen flag
+    pThis->baseXPos = baseXPos;
+    pThis->baseYPos = baseYPos;
+    pThis->unk_0_24 = unk_0_24;
+    pThis->scrollLength = scrollLength;
+    pThis->scrollCursorStartOffset = scrollCursorStartOffset;
+    pThis->scrollCursorEndOffset = scrollCursorEndOffset;
+    pThis->totalItems = totalItems;
+    pThis->visibleItems = visibleItems;
+    pThis->scrollCursorYPos = baseYPos + scrollCursorStartOffset;
+    pThis->updateCursorVisualFlag = 1;
     pThis->unk_0_1 = 1;
 }
 
-// 15424
-s32 sub_08015424(struct Unk_08015384 *pThis)
+/**
+ * @brief 15424 | Get scroll cursor Y position
+ * 
+ * @param pThis Pointer to scrollbar
+ * @return s32 Scroll cursor Y position
+ */
+s32 GL_GetScrollCursorYPosition(struct ScrollBarInfo *pThis)
 {
     if (!(pThis))
     {
         OSi_Panic("GL/GL_Scrollbar.c", 0x38, "pThis", 1);
     }
-    return pThis->unk_C_9;
+
+    return pThis->scrollCursorYPos;
 }
 
-// 1544C
-s32 sub_0801544C(struct Unk_08015384 *pThis)
+/**
+ * @brief 1544C | Calculates the scroll cursor Y position based off the scroll bar data
+ * 
+ * @param pThis Pointer to scrollbar
+ * @return s32 Calculated scroll cursor Y position
+ */
+s32 GL_CalculateScrollCursorYPos(struct ScrollBarInfo *pThis)
 {
-    return pThis->unk_0_15 + pThis->unk_9 + (((pThis->unk_8 - (pThis->unk_9 * 2) - pThis->unk_A) * pThis->unk_4) / (pThis->unk_6 - pThis->unk_B));
+    s32 scrollCursorStartPos;
+    s32 scrollCursorUsableLength;
+    s32 scrollPosOffset;
+
+    scrollCursorStartPos = pThis->baseYPos + pThis->scrollCursorStartOffset;
+    scrollCursorUsableLength = (pThis->scrollLength - (pThis->scrollCursorStartOffset * 2) - pThis->scrollCursorEndOffset);
+    scrollPosOffset = (scrollCursorUsableLength * pThis->scrollCursorIndex) / (pThis->totalItems - pThis->visibleItems);
+    return scrollCursorStartPos + scrollPosOffset;
 }
 
 // 15480
-s32 sub_08015480(struct Unk_08015384 *pThis)
+s32 sub_08015480(struct ScrollBarInfo *pThis)
 {
     return pThis->unk_0_2;
 }
 
-// 15488
-s32 sub_08015488(struct Unk_08015384 *pThis)
+/**
+ * @brief 15488 | Get update cursor visual flag
+ * 
+ * @param pThis Pointer to scrollbar
+ * @return s32 Update cursor visual flag
+ */
+s32 GL_GetUpdateCursorVisualFlag(struct ScrollBarInfo *pThis)
 {
-    return pThis->unk_0_0;
+    return pThis->updateCursorVisualFlag;
 }
 
-// 15490
-void sub_08015490(struct Unk_08015384 *pThis, s32 arg1)
+/**
+ * @brief 15490 | Set update cursor visual flag
+ * 
+ * @param pThis Pointer to scrollbar
+ */
+void GL_SetUpdateCursorVisualFlag(struct ScrollBarInfo *pThis, s32 flag)
 {
-    pThis->unk_0_0 = arg1;
+    pThis->updateCursorVisualFlag = flag;
 }
 
-// 154A4
-s32 sub_080154A4(struct Unk_08015384 *pThis)
+/**
+ * @brief 154A4 | Get scroll cursor index
+ * 
+ * @param pThis Pointer to scrollbar
+ * @return s32 Scroll cursor index
+ */
+s32 GL_GetScrollCursorIndex(struct ScrollBarInfo *pThis)
 {
     if (!(pThis))
     {
         OSi_Panic("GL/GL_Scrollbar.c", 0x5A, "pThis", 1);
     }
-    return pThis->unk_4;
+    return pThis->scrollCursorIndex;
 }
 
-// 154C8
-void sub_080154C8(struct Unk_08015384 *pThis, s32 arg1)
+/**
+ * @brief 154C8 | Set scroll cursor index
+ * 
+ * @param pThis Pointer to scrollbar
+ */
+void GL_SetScrollCursorIndex(struct ScrollBarInfo *pThis, s32 index)
 {
     if (!(pThis))
     {
         OSi_Panic("GL/GL_Scrollbar.c", 0x60, "pThis", 1);
     }
-    pThis->unk_4 = arg1;
+    pThis->scrollCursorIndex = index;
 }
 
-// 154F0
-s32 sub_080154F0(struct Unk_08015384 *pThis)
+/**
+ * @brief 154F0 | Check if the scroll cursor is not at the end
+ * 
+ * @param pThis Pointer to scrollbar
+ * @return s32 Bool, scroll cursor is not at the end
+ */
+s32 GL_CheckScrollCursorNotAtEnd(struct ScrollBarInfo *pThis)
 {
-    return (pThis->unk_4 < (pThis->unk_6 - pThis->unk_B));
+    return (pThis->scrollCursorIndex < (pThis->totalItems - pThis->visibleItems));
 }
 
-// 1550C
-s32 sub_0801550C(struct Unk_08015384 *pThis)
+/**
+ * @brief 1550C | Check if the scroll cursor is not at the beginning
+ * 
+ * @param pThis Pointer to scrollbar
+ * @return s32 Bool, scroll cursor is not at the beginning
+ */
+s32 GL_CheckScrollCursorNotAtBeginning(struct ScrollBarInfo *pThis)
 {
-    return (pThis->unk_4 != 0);
+    return (pThis->scrollCursorIndex != 0);
 }
 
-// 15518
-s32 sub_08015518(struct Unk_08015384 *pThis)
+/**
+ * @brief 15518 | Increase scroll cursor index by one unit
+ * 
+ * @param pThis Pointer to scrollbar
+ * @return s32 Bool, scroll cursor was able to increment
+ */
+s32 GL_IncreaseScrollCursorIndexByOne(struct ScrollBarInfo *pThis)
 {
     if (!(pThis))
     {
         OSi_Panic("GL/GL_Scrollbar.c", 0x75, "pThis", 1);
     }
 
-    if (pThis->unk_4 < (pThis->unk_6 - pThis->unk_B))
+    if (pThis->scrollCursorIndex < (pThis->totalItems - pThis->visibleItems))
     {
-        pThis->unk_4 += 1;
+        pThis->scrollCursorIndex += 1;
         return 1;
     }
 
     return 0;
 }
 
-// 15550
-s32 sub_08015550(struct Unk_08015384 *pThis)
+/**
+ * @brief 15550 | Decrease scroll cursor index by one unit
+ * 
+ * @param pThis Pointer to scrollbar
+ * @return s32 Bool, scroll cursor was able to decrement
+ */
+s32 GL_DecreaseScrollCursorIndexByOne(struct ScrollBarInfo *pThis)
 {
     if (!(pThis))
     {
         OSi_Panic("GL/GL_Scrollbar.c", 0x7F, "pThis", 1);
     }
 
-    if (pThis->unk_4 != 0)
+    if (pThis->scrollCursorIndex != 0)
     {
-        pThis->unk_4 -= 1;
+        pThis->scrollCursorIndex -= 1;
         return 1;
     }
 
     return 0;
 }
 
-// 15584
-void sub_08015584(struct Unk_08015384 *pThis)
+/**
+ * @brief 15584 | Increase scroll cursor index by one page
+ * 
+ * @param pThis Pointer to scrollbar
+ */
+void GL_IncreaseScrollCursorIndexByPage(struct ScrollBarInfo *pThis)
 {
-    s32 var_r0;
+    s32 maxScrollIndex;
 
     if (!(pThis))
     {
         OSi_Panic("GL/GL_Scrollbar.c", 0x89, "pThis", 1);
     }
 
-    pThis->unk_4 += pThis->unk_B;
-    var_r0 = pThis->unk_6 - pThis->unk_B;
-    if (pThis->unk_4 > var_r0)
+    pThis->scrollCursorIndex += pThis->visibleItems;
+    maxScrollIndex = pThis->totalItems - pThis->visibleItems;
+    if (pThis->scrollCursorIndex > maxScrollIndex)
     {
-        if (var_r0 < 0)
+        if (maxScrollIndex < 0)
         {
-            var_r0 = 0;
+            maxScrollIndex = 0;
         }
-        pThis->unk_4 = var_r0;
+        pThis->scrollCursorIndex = maxScrollIndex;
     }
 }
 
-// 155C0
-void sub_080155C0(struct Unk_08015384 *pThis)
+/**
+ * @brief 155C0 | Decrease scroll cursor index by one page
+ * 
+ * @param pThis Pointer to scrollbar
+ */
+void GL_DecreaseScrollCursorIndexByPage(struct ScrollBarInfo *pThis)
 {
     if (!(pThis))
     {
         OSi_Panic("GL/GL_Scrollbar.c", 0x90, "pThis", 1);
     }
 
-    if (pThis->unk_4 > pThis->unk_B)
+    if (pThis->scrollCursorIndex > pThis->visibleItems)
     {
-        pThis->unk_4 -= pThis->unk_B;
+        pThis->scrollCursorIndex -= pThis->visibleItems;
     }
     else
     {
-        pThis->unk_4 = 0;
+        pThis->scrollCursorIndex = 0;
     }
 }
 
-// 155F4
-void sub_080155F4(struct Unk_08015384 *pThis)
+/**
+ * @brief 155F4 | Update the scroll cursor Y position
+ * 
+ * @param pThis Pointer to scrollbar
+ */
+void GL_UpdateScrollCursorYPosition(struct ScrollBarInfo *pThis)
 {
     if (!(pThis))
     {
         OSi_Panic("GL/GL_Scrollbar.c", 0x9A, "pThis", 1);
     }
 
-    if (pThis->unk_0_0)
+    if (pThis->updateCursorVisualFlag)
     {
-        pThis->unk_C_9 = sub_0801544C(pThis);
+        pThis->scrollCursorYPos = GL_CalculateScrollCursorYPos(pThis);
     }
 }
